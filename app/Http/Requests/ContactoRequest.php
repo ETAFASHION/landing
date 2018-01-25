36e643405bd74acde7e1b951739ef1bb945b7dfd
Isaac Carrera <rolodef1@gmail.com>
2018-01-25 13:40:08 -0500

    Validacion influencers

--- app/Http/Requests/ContactoRequest.php
+++ app/Http/Requests/ContactoRequest.php
@@ -28,7 +28,7 @@ class ContactoRequest extends FormRequest
             'apellidos' => 'required|max:50',
             'email' => 'required|email|max:255|unique:contactos,email',
             'movil' => 'required|regex:/^[0-9]+$/|min:6|max:15',
-            'edad' => 'required|number',
+            'edad' => 'required',
             'ciudad' => 'required'
         ];
     }
